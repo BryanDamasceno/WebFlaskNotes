686f8c377b6054a68e674084735452a39da36164
Bryan Azevedo <bryan.azevedo@maistodos.com.br>
2024-03-10 00:54:23 -0300

    adding class to check input

--- website/auth.py
+++ website/auth.py
@@ -19,14 +19,11 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        firstName = request.form.get('firstName') 
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-        
-        validate_input.check_len_basics(email,firstName, password1)
+    if request.method == 'POST':                
+        validate_input.check_len_basics(request)
+        validate_input.signup_match_passwords(request)
  
+    flash('Account created!', category='success')
     return render_template("signup.html", user="bryan")
 
 
--- website/security/input_checks.py
+++ website/security/input_checks.py
@@ -1,15 +1,21 @@
 from flask import flash
 
 class InputValidation():
-    def check_len_basics(self,email, firstName, password1):
-        if len(email) < 7 or len(firstName) < 4 or len(password1) < 8:
+    def check_len_basics(self, request):
+        
+        self.email = request.form.get('email')
+        self.firstName = request.form.get('firstName') 
+        self.password1 = request.form.get('password1')
+        self.password2 = request.form.get('password2')
+
+        if len(self.email) < 7 or len(self.firstName) < 4 or len(self.password1) < 8:
 
             flash(f"Email must be greater than 4 characteres, " \
                    "First Name must be greater than 4 characteres and " 
                    "Password must be grater than 8 characteres", category='error')
 
 
-    def signup_match_passwords(self, password1, password2):
-        if password1 != password2:
-           flash("Password1 is different from Password2", category='error')
+    def signup_match_passwords(self, request):
+        if self.password1 != self.password2: 
+           flash("Passwords dont match", category='error')
     
